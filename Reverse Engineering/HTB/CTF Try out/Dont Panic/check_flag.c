c3eaef0c0593380d85e748c6ec79e6c3024faf4f
Thanh Nguyen <woodydadrillers0707@gmail.com>
2025-12-09 22:20:06 +0700

    Create check_flag.c

---
+++ Reverse Engineering/HTB/CTF Try out/Dont Panic/check_flag.c
@@ -0,0 +1,139 @@
+__int64 __fastcall src::check_flag::h397d174e03dc8c74(
+        __int64 a1,
+        __int64 a2,
+        __int64 a3,
+        __int64 a4,
+        __int64 a5,
+        __int64 a6)
+{
+  __int64 (__fastcall *v6)(); // rdx
+  __int64 (__fastcall *v7)(); // rcx
+  __int64 result; // rax
+  __int64 v9; // r14
+  __int64 v10; // [rsp+0h] [rbp-148h] BYREF
+  __int64 v11; // [rsp+8h] [rbp-140h] BYREF
+  __int64 (__fastcall *v12)(); // [rsp+10h] [rbp-138h]
+  __int64 (__fastcall *v13)(); // [rsp+18h] [rbp-130h]
+  __int64 (__fastcall *v14)(); // [rsp+20h] [rbp-128h]
+  __int64 (__fastcall *v15)(); // [rsp+28h] [rbp-120h]
+  __int64 (__fastcall *v16)(); // [rsp+30h] [rbp-118h]
+  __int64 (__fastcall *v17)(); // [rsp+38h] [rbp-110h]
+  __int64 (__fastcall *v18)(); // [rsp+40h] [rbp-108h]
+  __int64 (__fastcall *v19)(); // [rsp+48h] [rbp-100h]
+  __int64 (__fastcall *v20)(); // [rsp+50h] [rbp-F8h]
+  __int64 (__fastcall *v21)(); // [rsp+58h] [rbp-F0h]
+  __int64 (__fastcall *v22)(); // [rsp+60h] [rbp-E8h]
+  __int64 (__fastcall *v23)(); // [rsp+68h] [rbp-E0h]
+  __int64 (__fastcall *v24)(); // [rsp+70h] [rbp-D8h]
+  __int64 (__fastcall *v25)(); // [rsp+78h] [rbp-D0h]
+  __int64 (__fastcall *v26)(); // [rsp+80h] [rbp-C8h]
+  __int64 (__fastcall *v27)(); // [rsp+88h] [rbp-C0h]
+  __int64 (__fastcall *v28)(); // [rsp+90h] [rbp-B8h]
+  __int64 (__fastcall *v29)(); // [rsp+98h] [rbp-B0h]
+  __int64 (__fastcall *v30)(); // [rsp+A0h] [rbp-A8h]
+  __int64 (__fastcall *v31)(); // [rsp+A8h] [rbp-A0h]
+  __int64 (__fastcall *v32)(); // [rsp+B0h] [rbp-98h]
+  __int64 (__fastcall *v33)(); // [rsp+B8h] [rbp-90h]
+  __int64 (__fastcall *v34)(); // [rsp+C0h] [rbp-88h]
+  __int64 (__fastcall *v35)(); // [rsp+C8h] [rbp-80h]
+  __int64 (__fastcall *v36)(); // [rsp+D0h] [rbp-78h]
+  __int64 (__fastcall *v37)(); // [rsp+D8h] [rbp-70h]
+  __int64 (__fastcall *v38)(); // [rsp+E0h] [rbp-68h]
+  __int64 (__fastcall *v39)(); // [rsp+E8h] [rbp-60h]
+  __int64 (__fastcall *v40)(); // [rsp+F0h] [rbp-58h]
+  __int64 (__fastcall *v41)(); // [rsp+F8h] [rbp-50h]
+  __int64 (__fastcall *v42)(); // [rsp+100h] [rbp-48h]
+  _QWORD v43[8]; // [rsp+108h] [rbp-40h] BYREF
+
+  v12 = core::ops::function::FnOnce::call_once::h32497efb348ffe3c;
+  v13 = core::ops::function::FnOnce::call_once::h827ece763c8c7e2e;
+  v14 = core::ops::function::FnOnce::call_once::h784eba9476a4f0f4;
+  v15 = core::ops::function::FnOnce::call_once::hc26775751c1be756;
+  v16 = core::ops::function::FnOnce::call_once::hc599f6727ca8db95;
+  v17 = core::ops::function::FnOnce::call_once::h40d00bd196c3c783;
+  v18 = core::ops::function::FnOnce::call_once::h4e1d94269d5dab9f;
+  v19 = core::ops::function::FnOnce::call_once::h1e50475f0ef4e3b2;
+  v6 = core::ops::function::FnOnce::call_once::h28c42c5fb55e3f9f;
+  v20 = core::ops::function::FnOnce::call_once::h28c42c5fb55e3f9f;
+  v21 = core::ops::function::FnOnce::call_once::h08f069e45c38c91b;
+  v22 = core::ops::function::FnOnce::call_once::h70ddab66eb3eaf7e;
+  v23 = core::ops::function::FnOnce::call_once::h4e1d94269d5dab9f;
+  v24 = core::ops::function::FnOnce::call_once::h5935cc8a67508b36;
+  v25 = core::ops::function::FnOnce::call_once::h2ed86dfdd0fc9ca5;
+  v26 = core::ops::function::FnOnce::call_once::h28c42c5fb55e3f9f;
+  v27 = core::ops::function::FnOnce::call_once::h2ed86dfdd0fc9ca5;
+  v28 = core::ops::function::FnOnce::call_once::h70ddab66eb3eaf7e;
+  v29 = core::ops::function::FnOnce::call_once::h1e50475f0ef4e3b2;
+  v30 = core::ops::function::FnOnce::call_once::h2ed86dfdd0fc9ca5;
+  v31 = core::ops::function::FnOnce::call_once::h076f93abc7994a2b;
+  v32 = core::ops::function::FnOnce::call_once::h28c42c5fb55e3f9f;
+  v33 = core::ops::function::FnOnce::call_once::h1e50475f0ef4e3b2;
+  v34 = core::ops::function::FnOnce::call_once::h076f93abc7994a2b;
+  v35 = core::ops::function::FnOnce::call_once::ha0a2d91800448694;
+  v36 = core::ops::function::FnOnce::call_once::h28c42c5fb55e3f9f;
+  v37 = core::ops::function::FnOnce::call_once::hd3a717188d9c9564;
+  v38 = core::ops::function::FnOnce::call_once::h4aee5a63c69b281c;
+  v39 = core::ops::function::FnOnce::call_once::h4aee5a63c69b281c;
+  v7 = core::ops::function::FnOnce::call_once::h3dae80a6281f81f5;
+  v40 = core::ops::function::FnOnce::call_once::h3dae80a6281f81f5;
+  v41 = core::ops::function::FnOnce::call_once::h4aee5a63c69b281c;
+  v42 = core::ops::function::FnOnce::call_once::he29dc24b9b003076;
+  v10 = a2;
+  v11 = 31;
+  if ( a2 != 31 )
+  {
+    v43[0] = 0;
+    ((void (__fastcall __noreturn *)(__int64 *, __int64 *, _QWORD *))core::panicking::assert_failed::hb9915114bebb1f93)(
+      &v10,
+      &v11,
+      v43);
+  }
+  result = 0;
+  do
+  {
+    v9 = result + 1;
+    ((void (__fastcall *)(_QWORD, __int64, __int64 (__fastcall *)(), __int64 (__fastcall *)(), __int64, __int64, __int64, __int64, __int64 (__fastcall *)(), __int64 (__fastcall *)(), __int64 (__fastcall *)(), __int64 (__fastcall *)(), __int64 (__fastcall *)(), __int64 (__fastcall *)(), __int64 (__fastcall *)(), __int64 (__fastcall *)(), __int64 (__fastcall *)(), __int64 (__fastcall *)(), __int64 (__fastcall *)(), __int64 (__fastcall *)(), __int64 (__fastcall *)(), __int64 (__fastcall *)(), __int64 (__fastcall *)(), __int64 (__fastcall *)(), __int64 (__fastcall *)(), __int64 (__fastcall *)(), __int64 (__fastcall *)(), __int64 (__fastcall *)(), __int64 (__fastcall *)(), __int64 (__fastcall *)(), __int64 (__fastcall *)(), __int64 (__fastcall *)(), __int64 (__fastcall *)(), __int64 (__fastcall *)(), __int64 (__fastcall *)(), __int64 (__fastcall *)(), __int64 (__fastcall *)(), __int64 (__fastcall *)(), __int64 (__fastcall *)()))*(&v12 + result))(
+      *(unsigned __int8 *)(a1 + result),
+      31,
+      v6,
+      v7,
+      a5,
+      a6,
+      v10,
+      v11,
+      v12,
+      v13,
+      v14,
+      v15,
+      v16,
+      v17,
+      v18,
+      v19,
+      v20,
+      v21,
+      v22,
+      v23,
+      v24,
+      v25,
+      v26,
+      v27,
+      v28,
+      v29,
+      v30,
+      v31,
+      v32,
+      v33,
+      v34,
+      v35,
+      v36,
+      v37,
+      v38,
+      v39,
+      v40,
+      v41,
+      v42);
+    result = v9;
+  }
+  while ( v9 != 31 );
+  return result;
+}
